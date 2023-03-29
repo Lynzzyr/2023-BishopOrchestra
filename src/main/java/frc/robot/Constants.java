dadd0c3ae08622a6383f966c4351ea91533b057f
Jason Li <jasonli.0616@outlook.com>
2023-03-29 18:21:10 -0400

    Update auto names in code and doc

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -170,19 +170,19 @@ public final class Constants {
     public static final class kAutoRoutines {
 
         public static final class kOneConeOnePickup {
-            public static final String TURN_LEFT_place_pickup_balance = "1L. TURN LEFT place, pickup, balance";
-            public static final String TURN_RIGHT_place_pickup_balance = "1R. TURN RIGHT place, pickup, balance";
+            public static final String TURN_LEFT_place_pickup_balance = "A1L. TURN LEFT place, pickup, balance";
+            public static final String TURN_RIGHT_place_pickup_balance = "A1R. TURN RIGHT place, pickup, balance";
 
-            public static final String PLACE_CENTRE_drive_centre_pickup_centre_balance = "2. PLACE CENTRE, drive centre, pickup centre, balance";
+            public static final String CENTER_place_pickup_balance = "A2. CENTER place, pickup, balance";
 
-            public static final String TURN_LEFT_place_pickup_no_balance = "3L. TURN LEFT place, pickup, no balance";
-            public static final String TURN_RIGHT_place_pickup_no_balance = "3R. TURN RIGHT place, pickup, no balance";
+            public static final String TURN_LEFT_place_pickup_no_balance = "A3L. TURN LEFT place, pickup, no balance";
+            public static final String TURN_RIGHT_place_pickup_no_balance = "A3R. TURN RIGHT place, pickup, no balance";
 
             public static final String[] all = {
                 TURN_LEFT_place_pickup_balance,
                 TURN_RIGHT_place_pickup_balance,
 
-                PLACE_CENTRE_drive_centre_pickup_centre_balance,
+                CENTER_place_pickup_balance,
 
                 TURN_LEFT_place_pickup_no_balance,
                 TURN_RIGHT_place_pickup_no_balance
@@ -190,20 +190,16 @@ public final class Constants {
         }
         
         public static final class kOneConeAuto {
-            public static final String TURN_LEFT_place_and_balance = "4L. TURN LEFT place and balance";
-            public static final String TURN_RIGHT_place_and_balance = "4R. TURN RIGHT place and balance";
+            public static final String TURN_LEFT_place_and_balance = "B1L. TURN LEFT place and balance";
+            public static final String TURN_RIGHT_place_and_balance = "B1R. TURN RIGHT place and balance";
 
-            public static final String PLACE_SIDE_and_leave_community_no_balance = "5. PLACE SIDE and leave community, no balance";
-
-            public static final String PLACE_CENTRE_turn_LEFT_and_leave_community_no_balance = "6L. PLACE CENTRE, turn LEFT and leave community, no balance";
-            public static final String PLACE_CENTRE_turn_RIGHT_and_leave_community_no_balance = "6R. PLACE CENTRE, turn RIGHT and leave community, no balance";
+            public static final String PLACE_SIDE_and_leave_community_no_balance = "B2. PLACE SIDE and leave community, no balance";
 
             public static final String[] all = {
                 TURN_LEFT_place_and_balance,
                 TURN_RIGHT_place_and_balance,
-                PLACE_SIDE_and_leave_community_no_balance,
-                PLACE_CENTRE_turn_LEFT_and_leave_community_no_balance,
-                PLACE_CENTRE_turn_RIGHT_and_leave_community_no_balance
+                
+                PLACE_SIDE_and_leave_community_no_balance
             };
         }
     }
